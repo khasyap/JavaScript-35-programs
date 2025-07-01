4d8aee703059dd5d43fac86e242c22333c83bbe2
KONAKALLA KHASYAP SURYA SAKETH <118835857+khasyap@users.noreply.github.com>
2025-07-01 21:28:43 +0530

    prime

---
+++ prime.js
@@ -0,0 +1,14 @@
+function isPrime(number){
+    if(number<2)
+        return false;
+    for(let i=2;i<=Math.sqrt(number);i++){
+        if(number%i==0)
+            return false;
+    }
+    return true;
+}
+for (let i=0;i<=100;i++){
+    if(isPrime(i)){
+   console.log(i)
+    }
+}
